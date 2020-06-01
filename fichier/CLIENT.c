39d77c9a57c99e248f25a7da05601fe5abdb43ca
Ntumbabu <tshiteyababci@gmail.com>
2020-06-02 02:12:37 +0400

    Codage du fichier CLIENT.c

--- fichier/CLIENT.c
+++ fichier/CLIENT.c
@@ -1,11 +1,1 @@
-#include<sys/type.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<arpa/inet.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<pthread.h>
 
--- fichier/SERVEUR.c
+++ fichier/SERVEUR.c
@@ -1,4 +1,4 @@
-#include<sys/type.h>
+#include <sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
@@ -6,3 +6,45 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+
+
+typedef struct User
+{
+	char nom[30];
+	int age;
+}User;
+
+int main(void)
+{
+	int socketServer = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in addrServer;
+	addrServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addrServer.sin_family = AF_INET;
+	addrServer.sin_port = htons(30000);
+
+	bind(socketServer, (const struct sockaddr *)&addrServer, sizeof(addrServer));
+	printf("bind : %d\n", socketServer);
+
+	listen(socketServer, 3);
+	printf("listen\n");
+
+	struct sockaddr_in addrClient;
+	socklen_t csize = sizeof(addrClient);
+	int socketClient = accept(socketServer, (struct sockaddr *) &addrClient, &csize);
+	printf("accept\n");
+
+	printf("Client : %d\n", socketClient);
+
+	User user ={
+		.nom = "Arthur",
+		.age = 19
+	};
+	send(socketClient, &user, sizeof(user), 0);
+
+	close(socketClient);
+	close(socketServer);
+	printf("close\n");
+
+	return 0;
+}
